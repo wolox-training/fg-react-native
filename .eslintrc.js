c4bc04a7215f3784e1cd7ea1ebc342e2881a97f1
Santiago Fernandez <santiago.fernandez@wolox.com.ar>
2020-07-06 18:22:02 -0300

    Fixes and added configuration to base project

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,4 +3,14 @@ module.exports = {
   extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
+  rules: {
+    complexity: 'off',
+    'no-nested-ternary': 'off',
+    'no-magic-numbers': 'off',
+    'new-cap': 'off',
+    'comma-dangle': 'off',
+  },
+  settings: {
+    'import/ignore': ['node_modules'],
+  },
 };
